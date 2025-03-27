31840fea3dfc458abe51dc3069894cfa20670335
MansiMandre <mansimandre19@gmail.com>
2025-03-27 18:20:01 +0530

    Mansi_27/03

---
+++ src/Demo.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Edit from './Edit'
+
+const Demo = () => {
+
+  return (
+    <>
+    <Edit/>
+    <input type='text' placeholder='Enter your name'/>
+    <input type='text' placeholder='Enter your name'/>
+
+    </>
+  )
+}
+
+export default Demo
